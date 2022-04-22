db16194f021d5706c1db399c5b8c0287dbfe045e
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-04-22 12:02:52 +0200

    css

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -93,7 +93,7 @@ const Cart = (props) => {
             </button>
           </div>
           <div className=" overflow-auto">
-            <div className="table mt-4   overflow-scroll">
+            <div className="table w-full mt-4   overflow-scroll">
               <table className="w-full mt-8 cwish">
                 <thead>
                   <tr>
