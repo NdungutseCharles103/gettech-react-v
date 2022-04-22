e04e56ed00d84cd0b111440844ddcc99a79a139d
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-04-22 11:23:08 +0200

    css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import "tailboot/tailboot.css";
+import "./components/styles/tailboot.css";
+import "bootstrap/"
 import './components/styles/responsive.css'
 import App from './App';
 import '@fortawesome/fontawesome-free/css/all.css'
