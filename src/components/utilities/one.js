eeede16d550345d295ca8604fac28975a45303bc
Ndungutse Charles <ndungutsecharles103@gmail.com>
2023-01-12 08:35:17 +0200

    migrated backed server to railway

--- src/components/utilities/one.js
+++ src/components/utilities/one.js
@@ -2,22 +2,22 @@ import axios from "axios";
 import { getLocal } from "./useLocal";
 
 export const api = axios.create({
-  baseURL: `https://hitech1.herokuapp.com`,
+	baseURL: `https://hitech-backend.up.railway.app`,
 });
-//https://hitech1.herokuapp.com
+//https://hitech-backend.up.railway.app
 //http://localhost:8080
 export const Products = async () => {
-  const get = await api.get("/products");
-  const res = await get.data;
-  return res;
+	const get = await api.get("/products");
+	const res = await get.data;
+	return res;
 };
 
 export const payHandler = (fun, con, op) => {
-  if (op === "+") {
-    return fun((p) => p + con);
-  }else{
-    return fun((p) => p - con);
-  }
+	if (op === "+") {
+		return fun((p) => p + con);
+	} else {
+		return fun((p) => p - con);
+	}
 };
 
 export const PriceHandler = () => {};
@@ -25,36 +25,36 @@ export const PriceHandler = () => {};
 let price, Pname, image, category, desc;
 
 export const getprice = (e) => {
-  price = e.target.value;
+	price = e.target.value;
 };
 
 export const getPname = (e) => {
-  Pname = e.target.value;
+	Pname = e.target.value;
 };
 export const getimage = (e) => {
-  image = e.target.value;
+	image = e.target.value;
 };
 export const getcat = (e) => {
-  category = e.target.value;
+	category = e.target.value;
 };
 export const getdesc = (e) => {
-  desc = e.target.value;
+	desc = e.target.value;
 };
 
-const owner = getLocal("username")
+const owner = getLocal("username");
 
-export const submitProduct = async (e) =>{
-  e.preventDefault();
-  console.log(Pname, price, category, image, owner, desc);
-  const product = {
-    name: Pname,
-    price: price,
-    category: category,
-    image: image,
-    owner: owner,
-    description: desc
-  }
+export const submitProduct = async (e) => {
+	e.preventDefault();
+	console.log(Pname, price, category, image, owner, desc);
+	const product = {
+		name: Pname,
+		price: price,
+		category: category,
+		image: image,
+		owner: owner,
+		description: desc,
+	};
 
-  const done = await api.post('/products', product)
-  if(done) console.log(done);
-}
+	const done = await api.post("/products", product);
+	if (done) console.log(done);
+};
--- src/products/Products.jsx
+++ src/products/Products.jsx
@@ -11,248 +11,247 @@ import Footer from "../components/Sign/Footer";
 import { useSelector } from "react-redux";
 import { compareAndUpdate } from "../components/utilities/two";
 
-
 const Products = (props) => {
-const local = useSelector((state) => state.user.isLocal);
-  const [isProLoader, setProLoader] = useState(false);
-  const [sorted, setSorted] = React.useState("");
-  const {
-    cartCount,
-    filter,
-    userid,
-    payment,
-    setPayment,
-    counts,
-    updateCounts,
-    setFilter,
-    wishCount,
-    cartDecrement,
-    wishIncrement,
-    wishDecrement,
-    cartIncrement,
-    setProducts,
-    products,
-  } = props;
+	const local = useSelector((state) => state.user.isLocal);
+	const [isProLoader, setProLoader] = useState(false);
+	const [sorted, setSorted] = React.useState("");
+	const {
+		cartCount,
+		filter,
+		userid,
+		payment,
+		setPayment,
+		counts,
+		updateCounts,
+		setFilter,
+		wishCount,
+		cartDecrement,
+		wishIncrement,
+		wishDecrement,
+		cartIncrement,
+		setProducts,
+		products,
+	} = props;
 
-  useEffect(() => {
-    handleSorted();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filter, sorted]);
+	useEffect(() => {
+		handleSorted();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [filter, sorted]);
 
-  const handleSorted = () => {
-    switch (sorted) {
-      case "none":
-        setFilter(filter);
-        break;
+	const handleSorted = () => {
+		switch (sorted) {
+			case "none":
+				setFilter(filter);
+				break;
 
-      case "price":
-        setFilter(filter.sort(compDates));
-        break;
-      case "date":
-        setFilter(filter.sort(comPrice));
-        break;
+			case "price":
+				setFilter(filter.sort(compDates));
+				break;
+			case "date":
+				setFilter(filter.sort(comPrice));
+				break;
 
-      default:
-        setFilter(filter);
-        break;
-    }
-  };
-  console.log(filter);
-  const fetchProducts = async () => {
-    const data = await fetch("https://hitech1.herokuapp.com/products");
-    const pros = await data.json();
-    setProLoader(true);
-  };
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+			default:
+				setFilter(filter);
+				break;
+		}
+	};
+	console.log(filter);
+	const fetchProducts = async () => {
+		const data = await fetch("https://hitech-backend.up.railway.app/products");
+		const pros = await data.json();
+		setProLoader(true);
+	};
+	useEffect(() => {
+		fetchProducts();
+	}, []);
 
-  return (
-    <>
-      <div className="mb-7">
-        <Nav className="z-10" cartCount={cartCount} wishCount={wishCount} />
-        <Filter products={products} />
-        <div className="w-full flex items-center justify-center">
-          <Sort
-            filter={products}
-            setFilter={setFilter}
-            sorted={sorted}
-            setSorted={setSorted}
-          />
-        </div>
-        {isProLoader ? (
-          <div className="grid px-2 pro auto-col grid-cols-6 gap-4">
-            {filter.map((product) => (
-              <Test
-                key={product._id}
-                product={product}
-                local={local}
-                userid={userid}
-                setProducts={setProducts}
-                products={products}
-                cartIncrement={cartIncrement}
-                cartDecrement={cartDecrement}
-                wishDecrement={wishDecrement}
-                payment={payment}
-                counts={counts}
-                updateCounts={updateCounts}
-                wishIncrement={wishIncrement}
-                filter={filter}
-                setFilter={setFilter}
-                setPayment={setPayment}
-                cartCount={cartCount}
-              />
-            ))}
-          </div>
-        ) : (
-          <ProLoader />
-        )}
-      </div>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<div className='mb-7'>
+				<Nav className='z-10' cartCount={cartCount} wishCount={wishCount} />
+				<Filter products={products} />
+				<div className='w-full flex items-center justify-center'>
+					<Sort
+						filter={products}
+						setFilter={setFilter}
+						sorted={sorted}
+						setSorted={setSorted}
+					/>
+				</div>
+				{isProLoader ? (
+					<div className='grid px-2 pro auto-col grid-cols-6 gap-4'>
+						{filter.map((product) => (
+							<Test
+								key={product._id}
+								product={product}
+								local={local}
+								userid={userid}
+								setProducts={setProducts}
+								products={products}
+								cartIncrement={cartIncrement}
+								cartDecrement={cartDecrement}
+								wishDecrement={wishDecrement}
+								payment={payment}
+								counts={counts}
+								updateCounts={updateCounts}
+								wishIncrement={wishIncrement}
+								filter={filter}
+								setFilter={setFilter}
+								setPayment={setPayment}
+								cartCount={cartCount}
+							/>
+						))}
+					</div>
+				) : (
+					<ProLoader />
+				)}
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default Products;
 
 function Test(props) {
-  const {
-    local,
-    userid,
-    product,
-    cartDecrement,
-    wishIncrement,
-    filter,
-    setFilter,
-    cartIncrement,
-    wishDecrement,
-  } = props;
+	const {
+		local,
+		userid,
+		product,
+		cartDecrement,
+		wishIncrement,
+		filter,
+		setFilter,
+		cartIncrement,
+		wishDecrement,
+	} = props;
 
-  const smChange =()=>{
-    setFilter(
-      filter.map((pro) => {
-        if (pro._id === product._id) {
-          return {
-            ...pro,
-            cart: !pro.cart,
-            quantity: pro.quantity
-          };
-        }
-        return pro;
-      })
-    );
-  }
+	const smChange = () => {
+		setFilter(
+			filter.map((pro) => {
+				if (pro._id === product._id) {
+					return {
+						...pro,
+						cart: !pro.cart,
+						quantity: pro.quantity,
+					};
+				}
+				return pro;
+			})
+		);
+	};
 
-  const cartHandler = async() => {
-    setFilter(
-      filter.map((pro) => {
-        if (pro._id === product._id) {
-          return {
-            ...pro,
-            cart: !pro.cart,
-          };
-        }
-        return pro;
-      })
-    );
-    if (!product.cart) {
-      cartIncrement(product.price);
-      product.cart = true;
-      const upPro = await compareAndUpdate(product, filter);
-      if (!local) {
-        await api.put(`/user/${userid}/newUpdates`, {
-          products: upPro,
-        });
-      } else {
-        localStorage.setItem("products", JSON.stringify(upPro));
-      }
-    } else {
-      cartDecrement(1, product.price);
-      product.cart = false;
-      const upPro = await compareAndUpdate(product, filter);
-            if (!local) {
-              await api.put(`/user/${userid}/newUpdates`, {
-                products: upPro,
-              });
-            } else {
-              localStorage.setItem("products", JSON.stringify(upPro));
-            }
-    }
-  };
-  const wishHandler = async() => {
-    setFilter(
-      filter.map((pro) => {
-        if (pro._id === product._id) {
-          return {
-            ...pro,
-            wish: !pro.wish,
-          };
-        }
-        return pro;
-      })
-    );
-    if (!product.wish) {
-      wishIncrement();
-      product.wish = true;
-      const upPro = await compareAndUpdate(product, filter);
-            if (!local) {
-              await api.put(`/user/${userid}/newUpdates`, {
-                products: upPro,
-              });
-            } else {
-              localStorage.setItem("products", JSON.stringify(upPro));
-            }
-    } else {
-      wishDecrement();
-      product.wish = false;
-      const upPro = await compareAndUpdate(product, filter);
-            if (!local) {
-              await api.put(`/user/${userid}/newUpdates`, {
-                products: upPro,
-              });
-            } else {
-              localStorage.setItem("products", JSON.stringify(upPro));
-            }
-    }
-  };
-  const cartBtnClass = product.cart ? "bx bx-x added" : "bx bx-cart-add";
-  const cartBtnTitle = product.cart ? "view cart" : "add to cart";
-  const wishBtnClass = product.wish
-    ? "bx bx-list-minus added wish"
-    : "bx bx-list-plus";
-  const wishBtnTitle = product.wish
-    ? "remove from wishlist"
-    : "add to wishlist";
-  return (
-    <div
-      className="card text-sm  justify-between  bg-slate-100 cursor-pointer hover:scale-[1.05] duration-300 shadow-xl
-mt-3 flex flex-col items-center p-2 w-full"
-    >
-      <Link
-        title="click for more details"
-        to={`/preview/${product._id}`}
-        className="flex h-[65%] items-center flex-col w-full bg-white"
-      >
-        <img className="h-full" src={product.image} alt="" />
-      </Link>
-      <Link to={`/preview/${product._id}`} className="py-2 text-center">
-        {product.name}
-      </Link>
-      <div className="acts mx-auto flex items-center w-full px-2 justify-between">
-        <button
-          onClick={wishHandler}
-          title={wishBtnTitle}
-          className={`flex rbtns p-2 text-3xl bg-slate-300 items-center rounded-full  ${wishBtnClass}`}
-        ></button>
-        <Link to={`/preview/${product._id}`} className="flex items-center">
-          ${product.price}
-        </Link>
-        <button
-          onClick={cartHandler}
-          title={cartBtnTitle}
-          className={`flex lbtns text-3xl items-center p-2 ml-2 bg-blue-600 rounded-full ${cartBtnClass}`}
-        ></button>
-      </div>
-    </div>
-  );
+	const cartHandler = async () => {
+		setFilter(
+			filter.map((pro) => {
+				if (pro._id === product._id) {
+					return {
+						...pro,
+						cart: !pro.cart,
+					};
+				}
+				return pro;
+			})
+		);
+		if (!product.cart) {
+			cartIncrement(product.price);
+			product.cart = true;
+			const upPro = await compareAndUpdate(product, filter);
+			if (!local) {
+				await api.put(`/user/${userid}/newUpdates`, {
+					products: upPro,
+				});
+			} else {
+				localStorage.setItem("products", JSON.stringify(upPro));
+			}
+		} else {
+			cartDecrement(1, product.price);
+			product.cart = false;
+			const upPro = await compareAndUpdate(product, filter);
+			if (!local) {
+				await api.put(`/user/${userid}/newUpdates`, {
+					products: upPro,
+				});
+			} else {
+				localStorage.setItem("products", JSON.stringify(upPro));
+			}
+		}
+	};
+	const wishHandler = async () => {
+		setFilter(
+			filter.map((pro) => {
+				if (pro._id === product._id) {
+					return {
+						...pro,
+						wish: !pro.wish,
+					};
+				}
+				return pro;
+			})
+		);
+		if (!product.wish) {
+			wishIncrement();
+			product.wish = true;
+			const upPro = await compareAndUpdate(product, filter);
+			if (!local) {
+				await api.put(`/user/${userid}/newUpdates`, {
+					products: upPro,
+				});
+			} else {
+				localStorage.setItem("products", JSON.stringify(upPro));
+			}
+		} else {
+			wishDecrement();
+			product.wish = false;
+			const upPro = await compareAndUpdate(product, filter);
+			if (!local) {
+				await api.put(`/user/${userid}/newUpdates`, {
+					products: upPro,
+				});
+			} else {
+				localStorage.setItem("products", JSON.stringify(upPro));
+			}
+		}
+	};
+	const cartBtnClass = product.cart ? "bx bx-x added" : "bx bx-cart-add";
+	const cartBtnTitle = product.cart ? "view cart" : "add to cart";
+	const wishBtnClass = product.wish
+		? "bx bx-list-minus added wish"
+		: "bx bx-list-plus";
+	const wishBtnTitle = product.wish
+		? "remove from wishlist"
+		: "add to wishlist";
+	return (
+		<div
+			className='card text-sm  justify-between  bg-slate-100 cursor-pointer hover:scale-[1.05] duration-300 shadow-xl
+mt-3 flex flex-col items-center p-2 w-full'
+		>
+			<Link
+				title='click for more details'
+				to={`/preview/${product._id}`}
+				className='flex h-[65%] items-center flex-col w-full bg-white'
+			>
+				<img className='h-full' src={product.image} alt='' />
+			</Link>
+			<Link to={`/preview/${product._id}`} className='py-2 text-center'>
+				{product.name}
+			</Link>
+			<div className='acts mx-auto flex items-center w-full px-2 justify-between'>
+				<button
+					onClick={wishHandler}
+					title={wishBtnTitle}
+					className={`flex rbtns p-2 text-3xl bg-slate-300 items-center rounded-full  ${wishBtnClass}`}
+				></button>
+				<Link to={`/preview/${product._id}`} className='flex items-center'>
+					${product.price}
+				</Link>
+				<button
+					onClick={cartHandler}
+					title={cartBtnTitle}
+					className={`flex lbtns text-3xl items-center p-2 ml-2 bg-blue-600 rounded-full ${cartBtnClass}`}
+				></button>
+			</div>
+		</div>
+	);
 }
